b2ca01af89a70736949752c9d76ed7709eff8f8e
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-12 10:33:46 +0100

    decimal two

---
+++ ex05/src/index.js
@@ -0,0 +1,36 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne = 2.5;
+var numTwo = 2.0;
+var product = numOne * numTwo;
+    // Only change code below this line
+    return product;
+}
+
+console.log(main(2.5, 2.0));
+module.exports = main;
+
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne = 5.5;
+var numTwo = 5.5;
+var product = numOne * numTwo;
+    // Only change code below this line
+    return product;
+}
+
+console.log(main(5.5, 2.5));
+module.exports = main;
+
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne = 100.25;
+var numTwo = 43.20;
+var product = numOne * numTwo;
+    // Only change code below this line
+    return product;
+}
+
+console.log(main(100.25, 43.20));
+module.exports = main;
+
