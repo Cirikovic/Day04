d15b6e0bacdf7f69aeadd6fddd00a107adfbab7d
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-12 14:19:08 +0100

    name, lastname, town

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    
+    // Only change code below this line
+    var myFirstName = "Almir";
+    var myLastName = "Cirikovic";
+    var myHomeTown = "Vares";
+    
+    // Only change code below this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
