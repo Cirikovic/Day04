ee26d3ec96e9e4d8fd7303683b573b28c9678ed3
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-12 10:07:51 +0100

    decimal

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+var myDecimal = 8.2;
+var myDecimalE = myDecimal / 0.00254;
+    // Only change code below this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
+
