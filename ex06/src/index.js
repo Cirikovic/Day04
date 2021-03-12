184209e78e09051baeeb83a62243a926ccab559b
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-12 10:42:18 +0100

    decimal /

---
+++ ex06/src/index.js
@@ -0,0 +1,36 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne = 5.0;
+var numTwo = 2.0;
+var myQuotient = numOne / numTwo;
+    // Only change code below this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0));
+module.exports = main;
+
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne = 16.5;
+var numTwo = 5.5;
+var myQuotient = numOne / numTwo;
+    // Only change code below this line
+    return myQuotient;
+}
+
+console.log(main(16.5, 5.5));
+module.exports = main;
+
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne = 102.0;
+var numTwo = 25.5;
+var myQuotient = numOne / numTwo;
+    // Only change code below this line
+    return myQuotient;
+}
+
+console.log(main(102.0, 25.5));
+module.exports = main;
+
