a17a084f32018f1ea0a143aeef47f0b5d42cf725
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-12 09:21:27 +0100

    quotient

---
+++ ex01/src/index.js
@@ -0,0 +1,105 @@
+function main(numOne,numTwo) {
+    var quotient;
+    if(numTwo === 0) {
+        quotient = "Divide by 0! Cannot divide by zero";
+
+    } else {
+        var numOne = 15;
+        var numTwo = 3;
+        var quotient = numOne / numTwo;
+    }
+return quotient;
+
+}
+
+console.log(main(15,3));
+module.exports = main;
+
+function main(num1,num2) {
+    //Only change code below this line
+    var quotient;
+    if(num2 === 0) {
+        quotient = "Divide by 0! Cannot divide by zero";
+
+    } else {
+        var num1 = 25;
+        var num2 = 5;
+        var quotient = num1 / num2;
+    }
+return quotient;
+}
+
+console.log(main(25,5));
+module.exports = main;
+
+
+
+function main(num3,num4) {
+    //Only change code below this line
+    var quotient;
+    if(num4 === 0) {
+        quotient = "Divide by 0! Cannot divide by zero";
+
+    } else {
+        var num3 = 420;
+        var num4 = 10;
+        var quotient = num3 / num4;
+    }
+return quotient;
+}
+
+console.log(main(420,10));
+module.exports = main;
+
+function main(numOne,numTwo) {
+    var quotient;
+    if(numTwo === 0) {
+        quotient = "Divide by 0! Cannot divide by zero";
+
+    } else {
+        var numOne = 15;
+        var numTwo = 3;
+        var quotient = numOne / numTwo;
+    }
+return quotient;
+
+}
+
+console.log(main(15,3));
+module.exports = main;
+
+function main(num1,num2) {
+    //Only change code below this line
+    var quotient;
+    if(num2 === 0) {
+        quotient = "Divide by 0! Cannot divide by zero";
+
+    } else {
+        var num1 = 25;
+        var num2 = 5;
+        var quotient = num1 / num2;
+    }
+return quotient;
+}
+
+console.log(main(25,5));
+module.exports = main;
+
+
+
+function main(num5,num6) {
+    //Only change code below this line
+    var quotient;
+    if(num6 === 0) {
+        quotient = "Divide by 0! Cannot divide by zero";
+
+    } else {
+        var num5 = 55;
+        var num6 = 0;
+        var quotient = num5 / num6;
+    }
+return quotient;
+}
+
+console.log(main(55,0));
+module.exports = main;
